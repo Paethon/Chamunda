e4dc66e3bf96434abc70059ca2147cbfae24ebfe
Sebastian Stabinger <stabinger@gmail.com>
2013-08-19 16:40:01 +0200

    Implemented demotion, promotion

--- src/com/gmail/chamoners/chamunda/MobSpawner.scala
+++ src/com/gmail/chamoners/chamunda/MobSpawner.scala
@@ -6,9 +6,11 @@ import org.bukkit.entity.CreatureType
 import scala.collection.mutable.HashMap
 import org.bukkit.entity.LivingEntity
 
-
 case class Point(x: Int, z: Int)
-case class MobState(spawnPoint:Point)
+case class MobState(spawnPoint: Point) {
+  var promoted = false
+  var demoted = false
+}
 
 class MobSpawner(val center: Point,
                  val spawnsize: Int, val xCitySize: Int, val zCitySize: Int,
@@ -16,13 +18,14 @@ class MobSpawner(val center: Point,
   val spawnArray = Array.fill[Double](spawnsize, spawnsize)(1.0)
   val server = plugin.getServer
   val world = server.getWorlds.get(0)
-  val spawnPosition = HashMap.empty[LivingEntity, Point]
+  val spawnPosition = HashMap.empty[LivingEntity, MobState]
 
   // Set spawn probability inside city to zero
-  for(x <- (spawnsize/2)-(xCitySize/2) to (spawnsize/2)+(xCitySize/2);
-      z <- (spawnsize/2)-(zCitySize/2) to (spawnsize/2)+(zCitySize/2))
-    spawnArray(x)(z) = 0.0
-  
+  for (
+    x ← (spawnsize / 2) - (xCitySize / 2) until (spawnsize / 2) + (xCitySize / 2);
+    z ← (spawnsize / 2) - (zCitySize / 2) until (spawnsize / 2) + (zCitySize / 2)
+  ) spawnArray(x)(z) = 0.0
+
   private def spawnPoint = {
     var probability = 0.0
     var x = 0
@@ -42,12 +45,36 @@ class MobSpawner(val center: Point,
     world.spawnCreature(world.getHighestBlockAt(p.x, p.z).getLocation,
       creatureType)
   }
-  
-  def promot(e:LivingEntity) {
-    ???
+
+  /**
+   * Multiplies all spawn points around center with value
+   * @param center Location in spawn array around which the values should be
+   * multiplied
+   * @param radius "Radius" around center to multiply
+   * @param value value with which the entries should be multiplied
+   */
+  private def multiply(center: Point, radius: Int, value: Double) {
+    for (
+      x ← center.x - radius until center.x + radius;
+      z ← center.z - radius until center.z + radius
+    ) spawnArray(x)(z) *= value
   }
-  
-  def demote(e:LivingEntity) {
-    ???
+  /**
+   * 
+   */
+  def promote(e: LivingEntity, radius: Int = 5, amount: Double = 1.2) {
+    val state = spawnPosition(e)
+    if (!state.promoted) {
+      multiply(state.spawnPoint, radius, amount)
+      state.promoted = true
+    }
+  }
+
+  def demote(e: LivingEntity, radius: Int = 5, amount: Double = 0.8) {
+    val state = spawnPosition(e)
+    if (!state.demoted) {
+      multiply(state.spawnPoint, radius, amount)
+      state.demoted = true
+    }
   }
 }
