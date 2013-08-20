75e0800ba12773ee10932c9a9c8c6661d801356b
pausogus <pausogus@gmail.com>
2013-08-20 13:01:03 +0200

    new package++

---
+++ src/com/gmail/chamoners/chamunda/task/MobControl.scala
@@ -0,0 +1,37 @@
+package com.gmail.chamoners.chamunda.task
+
+import com.gmail.chamoners.chamunda.Preamble._
+import scala.collection.JavaConverters._
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.LivingEntity
+import collection.mutable.HashSet
+import org.bukkit.entity.Entity
+import de.ntcomputer.minecraft.controllablemobs.api.ControllableMobs
+import com.gmail.chamoners.chamunda.Environment
+
+class MobController(env: Environment) {
+
+  val controlledEntities = HashSet.empty[Entity]
+
+  private def control {
+    if (env.world.getPlayers.size != 0) {
+      val entities = env.world.getEntities().asScala
+      val player = env.world.getPlayers.get(0)
+      for (e <- entities) {
+        if (!controlledEntities.contains(e) && e.getType == EntityType.ZOMBIE) {
+          controlledEntities += e
+          val le = e.asInstanceOf[LivingEntity]
+          val ce = ControllableMobs.getOrAssign(le)
+          ce.getAttributes().getMovementSpeedAttribute().setBasisValue(0.4)
+          ce.getActions().target(player)
+          ce.getActions().follow(player)
+        }
+      }
+    }
+  }
+
+  def attach {
+    env.execute(20) { control }
+    env.log.info("MobController attached")
+  }
+}
